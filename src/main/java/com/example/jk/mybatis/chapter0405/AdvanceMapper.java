7c5287a42df4a1acc3d9b5ebfe0d598d2bffed31
JUNGQUI <ljk2518@gmail.com>
2021-03-29 14:54:39 +0900

    @Repository, @Param, Mapper

--- src/main/java/com/example/jk/mybatis/chapter0405/AdvanceMapper.java
+++ src/main/java/com/example/jk/mybatis/chapter0405/AdvanceMapper.java
@@ -1,8 +1,11 @@
 package com.example.jk.mybatis.chapter0405;
 
+import com.example.jk.mybatis.chapter0405.domain.WrapperClass;
+import java.util.List;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdvanceMapper {
-
+  public List<WrapperClass> findAllByWrapperId(@Param("wrapperId") String id);
 }
